d3a5e75fc0af2446c309aef486319e765d9b6dd8
Jadsia <megankyoung@live.com>
2019-07-05 16:29:57 -0700

    added error messages and redirects

--- app/controllers/insturctor/courses_controller.rb
+++ app/controllers/insturctor/courses_controller.rb
@@ -7,7 +7,10 @@ class Insturctor::CoursesController < ApplicationController
 
     def create 
         @course = current_user.courses.create(course_params)
-        redirect_to instructor_course_path(@course)
+        if @course.valid?
+            redirect_to instructor_course_path(@course)
+        else
+            render :new, status: :unprocessable_entity
     end
 
     def show
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
     belongs_to :user
+
+    validates :title, presence: true
+    validates :description, presence: true
+    vlaidates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end

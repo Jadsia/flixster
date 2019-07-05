77d17bdc6a196f7b691f06a29f9ece59a66dcc43
Jadsia <megankyoung@live.com>
2019-07-05 16:23:40 -0700

    added course model and controllers and namespace and insturctor view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
   root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
+  
   root to: 'home#index'
 end
---
+++ db/migrate/20190705225433_drop_email.rb
@@ -0,0 +1,4 @@
+class DropEmail < ActiveRecord::Migration[5.2]
+  def change
+  end
+end

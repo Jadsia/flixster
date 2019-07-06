aa1f86d545876cdd7279d05178ac7b3942cd125b
Jadsia <megankyoung@live.com>
2019-07-05 21:34:21 -0700

    added secure user authentication to several controllers

--- app/controllers/insturctor/courses_controller.rb
+++ app/controllers/insturctor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Insturctor::CoursesController < ApplicationController
     before_action :authenticate_user!
-  
+    before_action :require_authorized_for_current_course, only: [:show]
     def new
         @course = Course.new
     end
@@ -11,13 +11,24 @@ class Insturctor::CoursesController < ApplicationController
             redirect_to instructor_course_path(@course)
         else
             render :new, status: :unprocessable_entity
+        end
     end
 
     def show
-        @course = Couse.find(params[:id])
     end
 
     private
+
+    def require_authorized_for_current_course
+        if current_course.user != current_user
+            render plain: 'Unauthorized', status: :unauthorized
+        end
+    end
+
+    helper_method :current_course
+    def current_course
+        @current_course ||= Course.find(params[:id])
+    end
 
     def course_params
         params.require(:course).permit(:title, :description, :cost)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :views
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   
